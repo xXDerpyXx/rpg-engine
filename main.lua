03bbd8ead64bc40b75ad5f0dc16fddec611c5af2
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-08-11 03:48:19 -0400

    done?

--- main.lua
+++ main.lua
@@ -6,6 +6,10 @@ function splitByLines(str)
 	return lines
 end
 
+function trim(s)
+  return s:match'^()%s*$' and '' or s:match'^%s*(.*%S)'
+end
+
 local story = "test"
 local file = io.open(story..".txt","r")
 local rawStory = file:read("*all")
@@ -20,18 +24,37 @@ local prompt = 1
 
 while running do
 	local tempspot = 1
-	while tonumber(tokens[tempspot]) ~= prompt do
-		if(tonumber(tokens[tempspot]) ~= prompt) then
-			repeat
-				tempspot = tempspot+1
-			until tokens[tempspot] == "end"
-		else
+	while tokens[tempspot] ~= "prompt "..prompt do
+		if tokens[tempspot] == "prompt "..prompt then
 			break
 		end
+		tempspot = tempspot+1
 	end
+	
+	local options = {}
+	local outcomes = {}
+	
+	
+		tempspot = tempspot+1
+		print(tempspot..":"..tokens[tempspot])
+		tempspot = tempspot+1
+		tempspot = tempspot+1
+		
+		local optionCount = 0
 	repeat
-		print(tokens[tempspot])
+		table.insert(options,tokens[tempspot])
+		optionCount = optionCount+1
+		print(optionCount..":"..tokens[tempspot])
+		tempspot = tempspot+1
+		table.insert(outcomes,tokens[tempspot])
 		tempspot = tempspot+1
 	until tokens[tempspot] == "end"
-	input = io.read("*line")
+	input = ""
+	while options[tonumber(input)]==nil do
+		input = io.read("*line")
+	end
+	prompt = outcomes[tonumber(input)]
+	if options[tonumber(input)] == "endstory" then
+		running = false
+	end
 end
